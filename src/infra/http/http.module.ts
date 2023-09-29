bb06bbe2ac8ba2ee40781dc41d5fa0564d1d5cce
Leonardo Maciel Deschamps <leonardomacieldeschamps@gmail.com>
2023-09-28 21:56:54 -0300

    Controller: Responder Pergunta

--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -3,41 +3,45 @@ import { Module } from '@nestjs/common'
 import { DatabaseModule } from '../database/database.module'
 import { CryptographyModule } from '../cryptography/cryptography.module'
 
-import { AuthenticateController } from './controllers/authenticate.controller'
 import { CreateAccountController } from './controllers/create-account.controller'
+import { AuthenticateController } from './controllers/authenticate.controller'
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
+import { GetQuestionBySlugController } from './controllers/get-question-by-slug.controller'
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { EditQuestionController } from './controllers/edit-question.controller'
 import { DeleteQuestionController } from './controllers/delete-question.controller'
-import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
-import { GetQuestionBySlugController } from './controllers/get-question-by-slug.controller'
+import { AnswerQuestionController } from './controllers/answer-question.controller'
 
 import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student'
 import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student'
+import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
+import { GetQuestionBySlugUseCase } from '@/domain/forum/application/use-cases/get-question-by-slug'
 import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
 import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-question'
 import { DeleteQuestionUseCase } from '@/domain/forum/application/use-cases/delete-question'
-import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
-import { GetQuestionBySlugUseCase } from '@/domain/forum/application/use-cases/get-question-by-slug'
+import { AnswerQuestionUseCase } from '@/domain/forum/application/use-cases/answer-question'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
   controllers: [
-    AuthenticateController,
     CreateAccountController,
+    AuthenticateController,
+    FetchRecentQuestionsController,
+    GetQuestionBySlugController,
     CreateQuestionController,
     EditQuestionController,
     DeleteQuestionController,
-    FetchRecentQuestionsController,
-    GetQuestionBySlugController,
+    AnswerQuestionController,
   ],
   providers: [
     RegisterStudentUseCase,
     AuthenticateStudentUseCase,
+    FetchRecentQuestionsUseCase,
+    GetQuestionBySlugUseCase,
     CreateQuestionUseCase,
     EditQuestionUseCase,
     DeleteQuestionUseCase,
-    FetchRecentQuestionsUseCase,
-    GetQuestionBySlugUseCase,
+    AnswerQuestionUseCase,
   ],
 })
 export class HttpModule {}
