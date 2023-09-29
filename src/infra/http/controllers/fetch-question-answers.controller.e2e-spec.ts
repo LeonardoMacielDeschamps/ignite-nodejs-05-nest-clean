bfedf0ca7f680700bcb92639f8681cacf1169bd3
Leonardo Maciel Deschamps <leonardomacieldeschamps@gmail.com>
2023-09-29 20:56:30 -0300

    Controller: Listar Respostas da Pergunta

---
+++ src/infra/http/controllers/fetch-question-answers.controller.e2e-spec.ts
@@ -0,0 +1,60 @@
+import { AppModule } from '@/infra/app.module'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+import { AnswerFactory } from 'test/factories/make-answer'
+import { QuestionFactory } from 'test/factories/make-question'
+import { StudentFactory } from 'test/factories/make-student'
+
+describe('Fetch question answers (e2e)', () => {
+  let app: INestApplication
+  let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
+  let answerFactory: AnswerFactory
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory, AnswerFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    studentFactory = moduleRef.get(StudentFactory)
+    questionFactory = moduleRef.get(QuestionFactory)
+    answerFactory = moduleRef.get(AnswerFactory)
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  test('[GET] /questions/:questionId/answers', async () => {
+    const { id: authorId } = await studentFactory.makePrismaStudent()
+
+    const accessToken = jwt.sign({ sub: authorId.toString() })
+
+    const { id: questionId } = await questionFactory.makePrismaQuestion({
+      authorId,
+    })
+
+    const [firstAnswer, secondAnswer] = await Promise.all([
+      answerFactory.makePrismaAnswer({ questionId, authorId }),
+      answerFactory.makePrismaAnswer({ questionId, authorId }),
+    ])
+
+    const response = await request(app.getHttpServer())
+      .get(`/questions/${questionId.toString()}/answers`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send()
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toEqual({
+      answers: expect.arrayContaining([
+        expect.objectContaining({ content: firstAnswer.content }),
+        expect.objectContaining({ content: secondAnswer.content }),
+      ]),
+    })
+  })
+})
--- src/infra/http/http.module.ts
+++ src/infra/http/http.module.ts
@@ -5,51 +5,63 @@ import { CryptographyModule } from '../cryptography/cryptography.module'
 
 import { CreateAccountController } from './controllers/create-account.controller'
 import { AuthenticateController } from './controllers/authenticate.controller'
-import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
-import { GetQuestionBySlugController } from './controllers/get-question-by-slug.controller'
+
 import { CreateQuestionController } from './controllers/create-question.controller'
 import { EditQuestionController } from './controllers/edit-question.controller'
 import { DeleteQuestionController } from './controllers/delete-question.controller'
+import { GetQuestionBySlugController } from './controllers/get-question-by-slug.controller'
+import { FetchRecentQuestionsController } from './controllers/fetch-recent-questions.controller'
+
 import { AnswerQuestionController } from './controllers/answer-question.controller'
 import { EditAnswerController } from './controllers/edit-answer.controller'
 import { DeleteAnswerController } from './controllers/delete-answer.controller'
+import { FetchQuestionAnswersController } from './controllers/fetch-question-answers.controller'
 
 import { RegisterStudentUseCase } from '@/domain/forum/application/use-cases/register-student'
 import { AuthenticateStudentUseCase } from '@/domain/forum/application/use-cases/authenticate-student'
-import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
-import { GetQuestionBySlugUseCase } from '@/domain/forum/application/use-cases/get-question-by-slug'
+
 import { CreateQuestionUseCase } from '@/domain/forum/application/use-cases/create-question'
 import { EditQuestionUseCase } from '@/domain/forum/application/use-cases/edit-question'
 import { DeleteQuestionUseCase } from '@/domain/forum/application/use-cases/delete-question'
+import { GetQuestionBySlugUseCase } from '@/domain/forum/application/use-cases/get-question-by-slug'
+import { FetchRecentQuestionsUseCase } from '@/domain/forum/application/use-cases/fetch-recent-questions'
+
 import { AnswerQuestionUseCase } from '@/domain/forum/application/use-cases/answer-question'
 import { EditAnswerUseCase } from '@/domain/forum/application/use-cases/edit-answer'
 import { DeleteAnswerUseCase } from '@/domain/forum/application/use-cases/delete-answer'
+import { FetchQuestionAnswersUseCase } from '@/domain/forum/application/use-cases/fetch-question-answers'
 
 @Module({
   imports: [DatabaseModule, CryptographyModule],
   controllers: [
     CreateAccountController,
     AuthenticateController,
-    FetchRecentQuestionsController,
-    GetQuestionBySlugController,
+
     CreateQuestionController,
     EditQuestionController,
     DeleteQuestionController,
+    GetQuestionBySlugController,
+    FetchRecentQuestionsController,
+
     AnswerQuestionController,
     EditAnswerController,
     DeleteAnswerController,
+    FetchQuestionAnswersController,
   ],
   providers: [
     RegisterStudentUseCase,
     AuthenticateStudentUseCase,
-    FetchRecentQuestionsUseCase,
-    GetQuestionBySlugUseCase,
+
     CreateQuestionUseCase,
     EditQuestionUseCase,
     DeleteQuestionUseCase,
+    GetQuestionBySlugUseCase,
+    FetchRecentQuestionsUseCase,
+
     AnswerQuestionUseCase,
     EditAnswerUseCase,
     DeleteAnswerUseCase,
+    FetchQuestionAnswersUseCase,
   ],
 })
 export class HttpModule {}
