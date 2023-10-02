90f8582ec8e01d3bbb863b4101b53d5bcd24c09d
Leonardo Maciel Deschamps <leonardomacieldeschamps@gmail.com>
2023-10-02 20:07:13 -0300

    Criando pergunta com anexos

--- src/infra/http/controllers/create-question.controller.e2e-spec.ts
+++ src/infra/http/controllers/create-question.controller.e2e-spec.ts
@@ -5,32 +5,40 @@ import { INestApplication } from '@nestjs/common'
 import { JwtService } from '@nestjs/jwt'
 import { Test } from '@nestjs/testing'
 import request from 'supertest'
+import { AttachmentFactory } from 'test/factories/make-attachment'
 import { StudentFactory } from 'test/factories/make-student'
 
 describe('Create question (e2e)', () => {
   let app: INestApplication
   let prisma: PrismaService
   let studentFactory: StudentFactory
+  let attachmentFactory: AttachmentFactory
   let jwt: JwtService
 
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule, DatabaseModule],
-      providers: [StudentFactory],
+      providers: [StudentFactory, AttachmentFactory],
     }).compile()
 
     app = moduleRef.createNestApplication()
     prisma = moduleRef.get(PrismaService)
     studentFactory = moduleRef.get(StudentFactory)
+    attachmentFactory = moduleRef.get(AttachmentFactory)
     jwt = moduleRef.get(JwtService)
 
     await app.init()
   })
 
   test('[POST] /questions', async () => {
-    const user = await studentFactory.makePrismaStudent()
+    const [user, firstAttachment, secondAttachment] = await Promise.all([
+      studentFactory.makePrismaStudent(),
+      attachmentFactory.makePrismaAttachment(),
+      attachmentFactory.makePrismaAttachment(),
+    ])
 
     const accessToken = jwt.sign({ sub: user.id.toString() })
+
     const title = 'New question'
     const content = 'Question content'
 
@@ -40,16 +48,34 @@ describe('Create question (e2e)', () => {
       .send({
         title,
         content,
+        attachments: [
+          firstAttachment.id.toString(),
+          secondAttachment.id.toString(),
+        ],
       })
 
     expect(response.statusCode).toBe(201)
 
     const questionOnDatabase = await prisma.question.findFirst({
+      include: {
+        attachments: {
+          select: {
+            id: true,
+          },
+        },
+      },
       where: {
         title,
       },
     })
 
     expect(questionOnDatabase).toBeTruthy()
+    expect(questionOnDatabase?.attachments).toHaveLength(2)
+    expect(questionOnDatabase?.attachments).toEqual(
+      expect.arrayContaining([
+        expect.objectContaining({ id: firstAttachment.id.toString() }),
+        expect.objectContaining({ id: secondAttachment.id.toString() }),
+      ]),
+    )
   })
 })
