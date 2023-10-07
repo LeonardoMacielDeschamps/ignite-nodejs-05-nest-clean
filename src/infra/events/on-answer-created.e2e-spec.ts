135d6acdfd613d7bb347aeb965809e85352e7656
Leonardo Maciel Deschamps <leonardomacieldeschamps@gmail.com>
2023-10-07 13:07:49 -0300

    Testes E2E de eventos de domínio

---
+++ src/infra/events/on-answer-created.e2e-spec.ts
@@ -0,0 +1,60 @@
+import { AppModule } from '@/infra/app.module'
+import { DatabaseModule } from '@/infra/database/database.module'
+import { PrismaService } from '@/infra/database/prisma/prisma.service'
+import { INestApplication } from '@nestjs/common'
+import { JwtService } from '@nestjs/jwt'
+import { Test } from '@nestjs/testing'
+import request from 'supertest'
+import { QuestionFactory } from 'test/factories/make-question'
+import { StudentFactory } from 'test/factories/make-student'
+import { waitFor } from 'test/utils/wait-for'
+
+describe('On answer created (e2e)', () => {
+  let app: INestApplication
+  let prisma: PrismaService
+  let studentFactory: StudentFactory
+  let questionFactory: QuestionFactory
+  let jwt: JwtService
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [AppModule, DatabaseModule],
+      providers: [StudentFactory, QuestionFactory],
+    }).compile()
+
+    app = moduleRef.createNestApplication()
+    prisma = moduleRef.get(PrismaService)
+    studentFactory = moduleRef.get(StudentFactory)
+    questionFactory = moduleRef.get(QuestionFactory)
+    jwt = moduleRef.get(JwtService)
+
+    await app.init()
+  })
+
+  it('should create a notification when answer is created', async () => {
+    const { id: authorId } = await studentFactory.makePrismaStudent()
+
+    const accessToken = jwt.sign({ sub: authorId.toString() })
+
+    const { id: questionId } = await questionFactory.makePrismaQuestion({
+      authorId,
+    })
+
+    await request(app.getHttpServer())
+      .post(`/questions/${questionId.toString()}/answers`)
+      .set('Authorization', `Bearer ${accessToken}`)
+      .send({
+        content: 'New answer',
+      })
+
+    await waitFor(async () => {
+      const notificationOnDatabase = await prisma.notification.findFirst({
+        where: {
+          recipientId: authorId.toString(),
+        },
+      })
+
+      expect(notificationOnDatabase).not.toBeNull()
+    })
+  })
+})
